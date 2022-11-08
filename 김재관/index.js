9ef9dbe40083bbe98c0a6e935a3c0fe57b5f3945
jaegwans <skhu201811140@gmail.com>
2022-11-08 11:03:27 +0900

    refactor: js 함수 구조 변경

--- 김재관/index.js
+++ 김재관/index.js
@@ -7,32 +7,41 @@ const btnDe = document.querySelectorAll('.js-btn-de');
 
 // * 세터 게터 구현 및 함수 분할 예정
 
-const init = () => {
-    btns.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            if (inputString.innerHTML === '0') {
-                inputString.innerHTML = '';
-                inputString.innerHTML += btn.innerHTML;
-            } else {
-                inputString.innerHTML += btn.innerHTML;
-            }
-        });
-    });
-    btnAc.addEventListener('click', () => {
-        inputString.innerHTML = '0';
-        result.innerHTML = '0';
-    });
-    btnResult.addEventListener('click', () => {
-        inputString.innerHTML = eval(inputString.innerHTML);
-        result.innerHTML = eval(inputString.innerHTML);
-    });
-    btnDe.forEach((btn) => {
-        btn.addEventListener('click', warring);
-    });
+const btnClickHandler = (e) => {
+    const { target } = e;
+    if (inputString.innerHTML === '0') {
+        inputString.innerHTML = '';
+        inputString.innerHTML += target.innerHTML;
+        console.log(target);
+    } else {
+        inputString.innerHTML += target.innerHTML;
+        console.log(target);
+    }
+};
+
+const resultbtnClickHandler = () => {
+    inputString.innerHTML = eval(inputString.innerHTML);
+    result.innerHTML = eval(inputString.innerHTML);
+};
+
+const acBtnClickHandler = () => {
+    inputString.innerHTML = '0';
+    result.innerHTML = '0';
 };
 
 const warring = () => {
     alert('비활성화 된 버튼입니다.');
+};
+
+const init = () => {
+    btns.forEach((btn) => {
+        btn.addEventListener('click', btnClickHandler);
+    });
+    btnDe.forEach((btn) => {
+        btn.addEventListener('click', warring);
+    });
+    btnAc.addEventListener('click', acBtnClickHandler);
+    btnResult.addEventListener('click', resultbtnClickHandler);
 };
 
 init();
