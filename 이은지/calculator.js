3e5f9f7e00985f1eb71f4ac83ea00abd5116d891
eunji <eunjijjang11@naver.com>
2022-11-08 15:50:06 +0900

    docs: js 함수 한 번에 정의

--- 이은지/calculator.js
+++ 이은지/calculator.js
@@ -1,19 +1,19 @@
+const display = document.querySelector('.js-display');
+const result = document.querySelector('.js-result');
+
 // input에 클릭하면 해당 숫자, 문자 넣어주기
 function add(char) {
-    const display = document.getElementById('js-display');
     display.value = display.value + char;
 }
-//! querySelector로 하면 add는 실행되는데 calculate는 실행 안 됨
 
 // eval로 입력된 값 자동 계산
 function calculate() {
-    const display = document.getElementById('js-display');
-    const result = eval(display.value);
-    document.getElementById('js-result').value = result;
+    const results = eval(display.value);
+    result.value = results;
 }
 
 // value에 값 안 넣어서 reset
 function reset() {
-    document.getElementById('js-display').value = "";
-    document.getElementById('js-result').value = "";
+    display.value = "";
+    result.value = "";
 }
