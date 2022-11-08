5c68b0c559d4ff2fdaca8cdf3666c188407cca1a
baeksehee <tpgmlqor@gmail.com>
2022-11-09 08:19:05 +0900

    feat:index.js파일 계산기 기능 추가

--- 백세희/index.js
+++ 백세희/index.js
@@ -1,4 +1,16 @@
-const 
-const
+const display = document.querySelector(".js-display");
+const result = document.querySelector(".js-result");
 
-const
+function add(char) {
+  display.value += char;
+}
+
+function calculate() {
+  const RESULT = eval(display.value);
+  document.querySelector(".js-result").value = RESULT;
+}
+
+function reset() {
+  display.value = "";
+  result.value = "";
+}
