8f443a293c32125fd29feb76600919645c9d12cb
Kim0426 <706shin1728@naver.com>
2022-11-09 21:33:44 +0900

    GDSC SKHU Web - 김효중 (#5)
    * docs:구현 목록 정리
    
    * Update Init.md
    
    * feat:기본뼈대 작성(HTML+CSS)
    
    * docs:배치 사용 방법 추가
    
    * feat:내가 입력한 수 보이는 기능 추가
    
    * feat:연산자 요소와 item요소 모두 가져오는 객체 모듈화
    
    * feat:eval함수로 수식 평가 + 평가의 값과 수식 저장하는 기능 추가
    
    * refactor:queryselectorall을 nodelist->배열로 바꿔줌

---
+++ 김효중/js/Operator.js
@@ -0,0 +1,34 @@
+/*
+=를 갖는 dom요소
+*/
+const FindEqualItem = document.querySelector('.item.OperCalculate');
+/*
+수식을 저장할 거임
+*/
+const StoreExpress = document.querySelector('.item.StoreExpresstion');
+/* 
+=가 아닌 item 전부 찾아서 클릭하면 문자열 덧붙임
+*/
+const FindAllItem = document.querySelectorAll('.item:not(.OperCalculate)');
+Array.from(FindAllItem).forEach((EachItem) => {
+  EachItem.addEventListener('click', () => {
+    FindAllItem[0].innerHTML += EachItem.innerHTML;
+  });
+});
+/*
+AC->누르면 둘 다 초기화 해줌
+*/
+const Init = document.querySelector('.item.OperInit');
+Init.addEventListener('click', () => {
+  FindAllItem[0].innerHTML = '';
+  StoreExpress.innerHTML = '';
+});
+
+/*
+eval의 결과값과 수식 저장
+*/
+FindEqualItem.addEventListener('click', () => {
+  const Result = eval(FindAllItem[0].innerHTML);
+  StoreExpress.innerHTML = '수식:' + FindAllItem[0].innerHTML;
+  FindAllItem[0].innerHTML = '결과:' + Result;
+});
