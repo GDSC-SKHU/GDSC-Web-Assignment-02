e50ffa2eb6d6bd88bf03f66c51f002a6d1d412ed
zoeyourlife <201814112@office.skhu.ac.kr>
2022-11-09 10:54:40 +0900

    feat: 계산기 페이지 제작

---
+++ 최재훈/index.js
@@ -0,0 +1,27 @@
+// 결과 값
+const resultDisplay = document.querySelector(".js-result");
+
+// 내가 입력하는 값
+const inputDisplay = document.querySelector(".js-input");
+
+// AC, Del, = 제외 버튼 클릭 시 inputDisplay에 보여주기 위함
+function add(char) {
+  inputDisplay.value += char;
+}
+
+// "=" 클릭 시 계산 하는 기능
+function equal() {
+  const result = eval(inputDisplay.value);
+  resultDisplay.value = result;
+}
+
+// AC버튼 클릭 시 reset() 함수 실행
+function reset() {
+  document.querySelector(".js-result").value = "";
+  document.querySelector(".js-input").value = "";
+}
+
+// del 버튼 클릭 시 del() 함수 실행 slice 사용
+function del(char) {
+  inputDisplay.value = inputDisplay.value.slice(0, -1);
+}
