6a5a43147920d9d674a07fc64094d450058ca4bb
Jaegwan_kim <50818389+jaegwans@users.noreply.github.com>
2022-11-09 21:32:38 +0900

    GDSC SKHU Web - 김재관 (#2)
    * style: 초기 레이어웃 스타일링
    
    * style: btn 클릭 스타일링
    
    * feat: 초기 세팅
    
    * test: git test
    
    * feat: 계산기 기능 구현
    
    * refactor: js 함수 구조 변경
    
    * refactor: warring_msg 추가
    
    * refactor: 불필요 주석 제거

---
+++ 김재관/index.js
@@ -0,0 +1,44 @@
+const btns = document.querySelectorAll('.js-btn');
+const inputString = document.querySelector('.js-input-string');
+const result = document.querySelector('.js-result');
+const btnAc = document.querySelector('.js-btn-ac');
+const btnResult = document.querySelector('.js-btn-result');
+const btnDe = document.querySelectorAll('.js-btn-de');
+const WARRING_MSG = '비활성화 된 버튼입니다.';
+
+const btnClickHandler = (e) => {
+    const { target } = e;
+    if (inputString.innerHTML === '0') {
+        inputString.innerHTML = '';
+        inputString.innerHTML += target.innerHTML;
+    } else {
+        inputString.innerHTML += target.innerHTML;
+    }
+};
+
+const resultbtnClickHandler = () => {
+    inputString.innerHTML = eval(inputString.innerHTML);
+    result.innerHTML = eval(inputString.innerHTML);
+};
+
+const acBtnClickHandler = () => {
+    inputString.innerHTML = '0';
+    result.innerHTML = '0';
+};
+
+const warring = () => {
+    alert(WARRING_MSG);
+};
+
+const init = () => {
+    btns.forEach((btn) => {
+        btn.addEventListener('click', btnClickHandler);
+    });
+    btnDe.forEach((btn) => {
+        btn.addEventListener('click', warring);
+    });
+    btnAc.addEventListener('click', acBtnClickHandler);
+    btnResult.addEventListener('click', resultbtnClickHandler);
+};
+
+init();
